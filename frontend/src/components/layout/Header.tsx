b1062a52333d9907ad37ebe6c2171a3a4d11e513
Lee-chaerin <81899084+Lee-chaerin@users.noreply.github.com>
2025-11-18 07:43:23 +0900

    feat(frontend): 네비게이션에 'CS지식' 항목 추가

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -15,10 +15,11 @@ const Header = () => {
     <div className="fixed top-0 left-0 w-full h-20 md:h-24 px-6 md:px-12 py-5 md:py-8 flex justify-between items-center hover:bg-[#ffffffe0] text-white hover:text-[#333333] z-50">
       <h1 className="text-3xl md:text-4xl font-hannaPro">우아한테크메일</h1>
       
-      <nav className="hidden md:flex justify-between items-center w-xs text-2xl font-hanna">
+      <nav className="hidden md:flex justify-between items-center w-sm lg:w-md text-2xl font-hanna">
         <a href='#'>백엔드</a>
         <a href='#'>프론트엔드</a>
         <a href='#'>안드로이드</a>
+        <a href='#'>CS지식</a>
       </nav>
 
       <div className='md:hidden'>
@@ -32,6 +33,7 @@ const Header = () => {
             <DropdownMenuItem className="text-lg font-hanna"><a href="#">백엔드</a></DropdownMenuItem>
             <DropdownMenuItem className="text-lg font-hanna"><a href="#">프론트엔드</a></DropdownMenuItem>
             <DropdownMenuItem className="text-lg font-hanna"><a href="#">안드로이드</a></DropdownMenuItem>
+            <DropdownMenuItem className="text-lg font-hanna"><a href="#">CS지식</a></DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
         
